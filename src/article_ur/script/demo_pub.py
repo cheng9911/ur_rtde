e113cbeb75959537d76916d1b2f95ae67d4d77b3
cheng9911 <1962945668@qq.com>
2023-04-06 17:24:21 +0800

    添加滤波之后

--- src/article_ur/script/demo_pub.py
+++ src/article_ur/script/demo_pub.py
@@ -7,6 +7,15 @@ import argparse
 import sys
 import csv
 from std_msgs.msg import Float64MultiArray
+from scipy import signal
+import numpy as np
+fs=250
+
+fn=40
+wn=(fn/fs)   # 2*(fn/fs[0])
+b, a = signal.butter(8, [0.2,0.8], 'bandpass')   
+
+
 rtde_r = rtde_receive.RTDEReceiveInterface("192.168.3.101")
 rtde_c=rtde_control.RTDEControlInterface("192.168.3.101")
 Read_flag=True
@@ -35,14 +44,31 @@ dt=0.05
 
 def talker():
     pub = rospy.Publisher('force', Float64MultiArray, queue_size=1)
+    pub_seg = rospy.Publisher('force_seg', Float64MultiArray, queue_size=1)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         force_data=Float64MultiArray()
-        # rtde_c.zeroFtSensor()
+        sig_force=Float64MultiArray()
+        rtde_c.zeroFtSensor()
         force_data.data=rtde_r.getActualTCPForce()
         print(force_data.data)
+        with open("滤波前力信息.csv","a",newline='') as g:
+            writer1 = csv.writer(g)
+            writer1.writerow(force_data.data)
+        print("*******************************")
+        
+        sig_data=np.array(force_data.data)
+        # print(sig_data)
+        sig_ecg = signal.filtfilt(b, a, sig_data,padlen=0)
+        sig_force.data=sig_ecg
+        print(sig_ecg)
+        with open("滤波后力信息.csv","a",newline='') as g:
+            writer1 = csv.writer(g)
+            writer1.writerow(sig_ecg)
+        print("***********************************")
         pub.publish(force_data)
+        pub_seg.publish(sig_force)
         rate.sleep()
 if __name__ == '__main__':
     # rtde_c.zeroFtSensor()
